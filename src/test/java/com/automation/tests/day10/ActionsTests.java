53dddcceb4b7b0cf452fbe364da184064a0cf393
cristi <luticr@gmail.com>
2020-03-18 20:11:19 -0400

    day 10 Actions

--- src/test/java/com/automation/tests/day10/ActionsTests.java
+++ src/test/java/com/automation/tests/day10/ActionsTests.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
@@ -29,19 +30,42 @@ public class ActionsTests {
 
         WebElement img1 = driver.findElement(By.xpath("(//img)[1]"));
         WebElement img2 = driver.findElement(By.xpath("(//img)[2]"));
+        WebElement img3 = driver.findElement(By.xpath("(//img)[3]"));
+
 
         //build() is needed when you have couple of actions
         //always end with perform()
+        // pause(1000). - like Thread.sleep(1000)
         actions.moveToElement(img1).
                 pause(1000).
                 moveToElement(img2).
-                build().perform();
+                pause(1000).
+                moveToElement(img3).
+                build().
+                perform();
+
+        //hover on the first image
+        //verify that "name: user1" is displayed
+
+        actions.moveToElement(img1).perform();
+        WebElement imgText1 = driver.findElement(By.xpath("//h5[text()='name: user1']"));
+
+        // verify that WebElement that contains the text is visible
+        Assert.assertTrue(imgText1.isDisplayed());
+
+        //move to the second image
+        actions.moveToElement(img2).perform();
+        WebElement imgText2 = driver.findElement(By.xpath("//h5[text()='name: user2']"));
+        Assert.assertTrue(imgText2.isDisplayed());
+
+
 
 
     }
 
     @AfterMethod
     public void teardown() {
+        BrowserUtils.wait(3);
         driver.quit();
      }
 }
