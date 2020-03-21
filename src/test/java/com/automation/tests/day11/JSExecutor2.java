f08ed7b050759300ac901a3eecc6ee40d8da41a7
cristi <luticr@gmail.com>
2020-03-21 12:41:28 -0400

    day 11 Js Executor

---
+++ src/test/java/com/automation/tests/day11/JSExecutor2.java
@@ -0,0 +1,76 @@
+package com.automation.tests.day11;
+
+import com.automation.utilities.BrowserUtils;
+import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class JSExecutor2 {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setup(){
+        driver = DriverFactory.createDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/");
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void verifyTitle() {
+        String expected = "Practice";
+        // we create javaScriptExecutor object by casting webdriver object
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        //executeScript -this method executes javascript code
+        //we provide js code as a String
+        //return document.title - javascript code
+        //document - represents HTML page
+        // .toString() - to avoid additional casting from Object to String
+
+  //      String actual = (String) js.executeScript("return document.title");
+        //or
+        String actual =js.executeScript("return document.title").toString();
+
+        Assert.assertEquals(actual, expected);
+
+    }
+    @Test
+    public void clickTest() {
+        WebElement link = driver.findElement(By.linkText("Multiple Buttons"));
+        //disable this click action, to perform it with js executor
+        //link.click();
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        // after " " you can list WebElements that will be used
+        //as part of your javascript code
+        //it's varargs, so you can list 0+
+        //arguments - listed after ,
+        // use index to get specific webelement
+        //WebElement arguments = {element, link, link2};
+        //from left - to right
+
+        js.executeScript("arguments[0].click()", link);
+
+        WebElement button6 = driver.findElement(By.id("disappearing_button"));
+
+        js.executeScript("arguments[0].click()", button6);
+
+        BrowserUtils.wait(2);
+
+        WebElement result = driver.findElement(By.id("result"));
+
+        Assert.assertEquals(result.getText(), "Now it's gone!");
+
+    }
+
+    @AfterMethod
+    public void teardown() {
+        BrowserUtils.wait(2);
+        driver.quit();
+    }
+}
