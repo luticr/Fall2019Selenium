0cf62208f323cd761460811e30a3fdf7243d8a12
cristi <luticr@gmail.com>
2020-03-22 14:03:12 -0400

    day 12 wait (implicit, explicit)

--- src/test/java/com/automation/tests/day11/WebTables.java
+++ src/test/java/com/automation/tests/day11/WebTables.java
@@ -48,6 +48,7 @@ public class WebTables {
         // ////tbody//tr - to get all rows from table body, excluding table header
         List<WebElement> rows = driver.findElements(By.xpath("//table[1]//tbody//tr"));
         //if we will get a size of this collection, it automatically equals to number of elements
+        //expected - 4 rows in the table
         Assert.assertEquals(rows.size(), 4);
 
     }
@@ -62,6 +63,59 @@ public class WebTables {
 
     }
 
+    /**
+     * Go to tables example page
+     * Delete record with jsmith@gmail.com email
+     * verify that number of rows is equals  to 3
+     * verify that jsmith@gmail.com doesn't exists any more in the table
+     */
+    @Test
+    public void DeleteRowTest () {
+        String xpath = "//table[1]//td[text()='jsmith@gmail.com']/..//a[text()='delete']";
+        driver.findElement(By.xpath(xpath)).click();
 
+        BrowserUtils.wait(3);
 
+        //get count of rows
+        int rowCount = driver.findElements(By.xpath("//table[1]//tbody//tr")).size();
+
+        Assert.assertEquals(rowCount, 3);
+
+        List<WebElement> emails = driver.findElements(By.xpath("//table[1]//td[text()='jsmith@gmail.com']"));
+
+        Assert.assertTrue(emails.isEmpty());
+
+    }
+
+    /**
+     * Let's write a function that will return column index based on column name
+     */
+    @Test
+    public void getColumnIndexByName() {
+        String columnName = "Email";
+        List<WebElement> columnNames = driver.findElements(By.xpath("//table[1]//th"));
+        int index = 0;
+        for (int i = 0; i < columnNames.size(); i++) {
+            String actualColumnName = columnNames.get(i).getText();
+            System.out.println(String.format("Column name: %s, position %s", actualColumnName, i));
+            if (actualColumnName.equals(columnName)) {
+                index = i + 1;
+                break;
+            }
+        }
+        Assert.assertEquals(index, 3);
+    }
+    @Test
+    public void getSpecificCell() {
+        String expected = "http://www.jdoe.com";
+
+        int row = 3;
+        int column = 5;
+
+        String xpath = "//table[1]//tbody//tr[" + row + "]//td[" + column + "]";
+        WebElement cell = driver.findElement(By.xpath(xpath));
+
+        Assert.assertEquals(cell.getText(), expected);
+    }
 }
+
