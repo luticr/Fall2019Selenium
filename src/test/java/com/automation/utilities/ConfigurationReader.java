08516080d15eb42bf9c09b53b589465c76f81b29
cristi <luticr@gmail.com>
2020-03-24 22:16:54 -0400

    day 13 System Properties

--- src/test/java/com/automation/utilities/ConfigurationReader.java
+++ src/test/java/com/automation/utilities/ConfigurationReader.java
@@ -6,30 +6,60 @@ import java.util.Properties;
 public class ConfigurationReader {
 
     private static Properties configFile;
+
+
+
     static {
+
         try {
+
             //location of properties file
+
             String path = System.getProperty("user.dir")+"/configuration.properties";
-            //get thet file as a stream
-            FileInputStream input =  new FileInputStream(path);
+
+            //get that file as a stream
+
+            FileInputStream input = new FileInputStream(path);
+
             //create object of Properties class
+
             configFile = new Properties();
+
             //load properties file into Properties object
+
             configFile.load(input);
-            //close the input stream at the end
+
+            //close the input s
+
             input.close();
+
         } catch (Exception e) {
+
             e.printStackTrace();
+
             throw new RuntimeException("Failed to load properties file!");
+
         }
+
+
+
     }
 
+
+
     /**
-     * This method returns properties value from configuration.properties file
-     * @param keyName
-     * @return
+
+     * This method returns property value from configuration.properties file
+
+     * @param keyName property name
+
+     * @return property value
+
      */
+
     public static String getProperty(String keyName) {
+
         return configFile.getProperty(keyName);
+
     }
 }
