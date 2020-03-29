2d910b5542c8be8470a1d3a36f3db209744ab767
cristi <luticr@gmail.com>
2020-03-29 16:20:45 -0400

    day 14 page object model

--- src/test/java/com/automation/utilities/DateTimeUtilities.java
+++ src/test/java/com/automation/utilities/DateTimeUtilities.java
@@ -3,6 +3,7 @@ package com.automation.utilities;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class DateTimeUtilities {
 
@@ -30,14 +31,14 @@ public class DateTimeUtilities {
 
     /**
      * This method returns difference between end and start time
-     * @param start
-     * @param end
-     * @param format
-     * @return
+     * @param start time
+     * @param end time
+     * @param format like h:m a
+     * @return difference between end time and start time as a long
      */
-    public int getTimeDifference (String start, String end, String format) {
+    public static long getTimeDifference (String start, String end, String format) {
         LocalTime startTime = LocalTime.parse(start, DateTimeFormatter.ofPattern(format));
         LocalTime endTime = LocalTime.parse(end, DateTimeFormatter.ofPattern(format));
-        return 0;
+        return ChronoUnit.HOURS.between(startTime, endTime);
     }
 }
