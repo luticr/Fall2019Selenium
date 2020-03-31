d76ff4847be39ae19a14f9bb004d72c9d65bd79d
cristi <luticr@gmail.com>
2020-03-31 14:24:11 -0400

    day 14 page object model

--- src/test/java/com/automation/homework/RandomSelection.java
+++ src/test/java/com/automation/homework/RandomSelection.java
@@ -10,6 +10,12 @@ import org.testng.annotations.Test;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * 1.go to http://samples.gwtproject.org/samples/Showcase/Showcase.html#!CwCheckBox2.Randomlyselect a checkbox.
+ * As soon as you check any day, print the name of the day and uncheck immediately.
+ * After you check and uncheck Friday for the third time, exit the program.
+ */
+
 public class RandomSelection {
 
     private WebDriver driver = Driver.getDriver();
@@ -22,6 +28,7 @@ public class RandomSelection {
 
         List<WebElement> checkboxes = driver.findElements(By.cssSelector("input[type='checkbox']"));
         List<WebElement> labels = driver.findElements(By.xpath("//input[@type='checkbox']/following-sibling::label"));
+
         int counter = 0;
 
         while (counter<3) {
@@ -32,6 +39,7 @@ public class RandomSelection {
             if (checkboxes.get(checkboxToSelect).isEnabled()) {
 
                 checkboxes.get(checkboxToSelect).click();
+
                 System.out.println("Selected :: " + labels.get(checkboxToSelect).getText());
 
                 checkboxes.get(checkboxToSelect).click();
